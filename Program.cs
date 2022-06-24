4dbc385370d2a84e514923c8b4006843d786f2ee
resul pala <resulpala@hotmail.com.tr>
2022-06-24 21:27:52 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        String inputString; 
+        
+        inputString = Console.ReadLine(); 
+        
+        Console.WriteLine("Hello, World.");
+        
+        Console.WriteLine(inputString);
+    }
+}
